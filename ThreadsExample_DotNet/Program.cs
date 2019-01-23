37ffc605548d1c804d439265c12d62a51e337f63
Omar Munguia <ingscomarmr@gmail.com>
2019-01-23 08:52:56 -0600

    Ejemplo sencillo de Threads

--- ThreadsExample_DotNet/Program.cs
+++ ThreadsExample_DotNet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadsExample_DotNet
 {
@@ -10,12 +11,36 @@ namespace ThreadsExample_DotNet
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Inician los Threads");
+
+            Thread task1 = new Thread(new ThreadStart(PrintTime));
+            task1.Start();
+
+            ThreadStart tStart = new ThreadStart(PrintTime2);
+            Thread task2 = new Thread(tStart);
+            task2.Start();
+
+            Console.WriteLine("Finaliza programa");
+            Console.ReadKey();
+        }
+
+        public static void PrintTime() {
+            for (int i = 1; i <= 10; i++) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("P1:{0}",DateTime.Now.ToString("hh:mm:ss"));
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void PrintTime2()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("P2:{0}", DateTime.Now.ToString("hh:mm:ss"));
+                Thread.Sleep(2000);
+            }
         }
     }
 }
